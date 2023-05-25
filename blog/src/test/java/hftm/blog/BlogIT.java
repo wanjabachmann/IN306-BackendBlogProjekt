9e805306670214bad67cd587f075c74d749dfd7e
wanjabachmann <wanja.bachmann@hotmail.com>
2023-05-25 19:09:24 +0200

    refactor: :fire: Removes unused files from previous tests and the default setup.

--- blog/src/test/java/hftm/blog/BlogIT.java
+++
@@ -1,8 +0,0 @@
-package hftm.blog;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class BlogIT extends BlogTest {
-    // Execute the same tests but in packaged mode.
-}
