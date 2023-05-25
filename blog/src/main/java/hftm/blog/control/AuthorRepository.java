7ddae15c60e750b67486c06fc938987dab9c4a46
wanjabachmann <wanja.bachmann@hotmail.com>
2023-05-25 23:00:17 +0200

    feat: :card_file_box: Add repository which implements PanacheRepository

---
+++ blog/src/main/java/hftm/blog/control/AuthorRepository.java
@@ -0,0 +1,10 @@
+package hftm.blog.control;
+
+import hftm.blog.entity.Author;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class AuthorRepository implements PanacheRepository<Author> {
+    
+}
