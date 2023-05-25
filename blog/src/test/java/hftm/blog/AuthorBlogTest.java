6faa0e02eba208699eca95dff599e187528bf0e7
wanjabachmann <wanja.bachmann@hotmail.com>
2023-05-25 23:01:33 +0200

    test: :white_check_mark: add the junit tests for the entities author and blog

---
+++ blog/src/test/java/hftm/blog/AuthorBlogTest.java
@@ -0,0 +1,36 @@
+package hftm.blog;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import hftm.blog.boundary.AuthorService;
+import hftm.blog.boundary.BlogService;
+import hftm.blog.entity.Author;
+import hftm.blog.entity.Blog;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+
+
+@QuarkusTest
+public class AuthorBlogTest {
+    @Inject
+    BlogService blogService;
+
+    @Inject
+    AuthorService authorService;
+
+    @Test
+    void addAuthorToBlog(){
+        // Arrange
+        Blog blog = new Blog("addAuthorToBlog", "Blog from addAuthroToBlog Junit test.");
+        Author author = new Author("Christoph", "Meier");
+    
+        // Act
+        blog.getAuthors().add(author);
+
+        // Assert
+        // blog.getAuthors().forEach(x -> System.out.println(x.getFirstname() + " " + x.getLastname()));
+        assertTrue(blog.getAuthors().contains(author));
+    }
+}
