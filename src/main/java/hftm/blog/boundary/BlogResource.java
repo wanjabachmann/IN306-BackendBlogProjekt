e815246825f53702224bbad5ca25401d132f6a11
wanjabachmann <wanja.bachmann@hotmail.com>
2023-06-20 22:53:11 +0200

    feat: :sparkles: Adds the methods and anotations to the BlogResource

--- src/main/java/hftm/blog/boundary/BlogResource.java
+++ src/main/java/hftm/blog/boundary/BlogResource.java
@@ -1,19 +1,55 @@
 package hftm.blog.boundary;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
+import java.util.List;
 
-@Path("/blog")
+import hftm.blog.control.AuthorService;
+import hftm.blog.control.BlogService;
+import hftm.blog.entity.Author;
+import hftm.blog.entity.Blog;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+
+@Path("/blogs")
+@ApplicationScoped
 public class BlogResource {
 
+    @Inject
+    BlogService blogService;
+
+    @Inject
+    AuthorService authorService;
+
+    // Diese Methode ist über eine http-GET-Anfrage erreichbar.
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String blog() {
-        return "Hello from RESTEasy Reactive";
-        
-        
-        //Blog blog = new Blog("Title", "Content of the Blog");
+    public List<Blog> getEntries() {
+        return this.blogService.getBlogs();
     }
+
+    // Diese Methode ist über eine http-POST-Anfrage erreichbar.
+    @POST
+    public void addBlog(Blog blog) {
+        this.blogService.addBlog(blog);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void removeBlog(Long id){
+        this.blogService.removeBlogById(id);
+    }
+
+
+    // Author Section
+/*     @GET
+    public List<Author> getAuthors() {
+        return this.authorService.getAuthors();
+    } */
+
+/*     @POST
+    public void addAuthor (Author author){
+        this.authorService.addAuthor(author);
+    } */
 }
