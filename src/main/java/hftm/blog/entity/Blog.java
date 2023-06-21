4edb4d62399bb0f3c57ec3749d5a2b12837b1e90
wanjabachmann <wanja.bachmann@hotmail.com>
2023-06-21 07:14:18 +0200

    style: :art: Removes unused imports

--- src/main/java/hftm/blog/entity/Blog.java
+++ src/main/java/hftm/blog/entity/Blog.java
@@ -1,17 +1,14 @@
 package hftm.blog.entity;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
