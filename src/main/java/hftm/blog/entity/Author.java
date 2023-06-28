c465137bdf27bae7ea7d23d181f77ad79d099a8b
wanjabachmann <wanja.bachmann@hotmail.com>
2023-06-28 22:08:19 +0200

    feat:

--- src/main/java/hftm/blog/entity/Author.java
+++ src/main/java/hftm/blog/entity/Author.java
@@ -1,5 +1,7 @@
 package hftm.blog.entity;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -25,15 +27,17 @@ public class Author {
 
     private String firstname;
     private String lastname;
+    private LocalDate creationDate;
 
     // authors --> Name of the Hashset on Blog.java
     @ManyToMany(mappedBy = "authors")
     private Set<Blog> blogs = new HashSet<>();
 
     // Custom Constructor
-    public Author(String firstname, String lastname) {
+    public Author(String firstname, String lastname, LocalDate creationDate) {
         this.firstname = firstname;
         this.lastname = lastname;
+        this.creationDate = creationDate;
     }
     
 }
