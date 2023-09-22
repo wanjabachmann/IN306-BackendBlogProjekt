f7d6c29ed97c94fbd8126b4207cfea3649f88c59
wanjabachmann <wanja.bachmann@hotmail.com>
2023-09-22 15:49:02 +0200

    fix: :memo: Update @GeneratedValue with autogeneration

--- src/main/java/hftm/blog/entity/Blog.java
+++ src/main/java/hftm/blog/entity/Blog.java
@@ -12,6 +12,7 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
@@ -31,7 +32,7 @@ import lombok.Setter;
 @Entity
 public class Blog {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
     private Long id;
 
     @NotBlank
--- src/main/java/hftm/blog/entity/Comment.java
+++ src/main/java/hftm/blog/entity/Comment.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import jakarta.json.bind.annotation.JsonbTransient;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -22,7 +23,7 @@ import lombok.Setter;
 @Entity
 public class Comment {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
     private Long id;
 
     @NotBlank
